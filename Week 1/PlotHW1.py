e305f30856b148f10d5097890d2ba538d261d09d
MercyJe <mercydaguebor@gmail.com>
2016-01-19 04:11:45 -0500

    Update PlotHW1.py

--- Week 1/PlotHW1.py
+++ Week 1/PlotHW1.py
@@ -1,20 +1,29 @@
 #import numpy and matlibplots module
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 #generate array of x values from 0 to 6pi
+xvalues = np.linspace(0, 6*np.pi, 100, endpoint=False)
 
-#calculate y values for sin wave
-
-#calculate y values for cosine wave
+#calculate y values for sine wave
+ysin = np.sin(xvalues)
+#calculatfor cosine wave y values 
+ycos = np.cos(xvalues)
 
 #plot x and y values for both functions 
-
 #set dashed line for sine wave and solide line for cosine wave
+plt.plot(xvalues, ysin, '--b', linewidth=1.5, label = 'sine')
+plt.plot(xvalues, ycos, '-g',linewidth=1.5, label = 'cosine')
+plt.grid()
+plt.xlabel('x')
+plt.ylabel('sine and cosine')
+plt.title('Two Trigonometric graphs on one page')
 
 #add legend
-
+plt.legend( loc='upper right')
+plt.show()
 
 #leave comment here with your information
-#Name:
-#Date:
+#Name: Mercy J. Daniel Aguebor
+#Date: 1 - 18 - 2016
 
